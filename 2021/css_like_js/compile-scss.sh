e08d21b53365a600f968c25d8291c20f6b374c19
shmakovpn <shmakovpn@yandex.ru>
2021-04-11 08:45:41 +0300

    custom checkbox day/night

--- 2021/css_like_js/compile-scss.sh
+++ 2021/css_like_js/compile-scss.sh
@@ -2,4 +2,5 @@
 # Author: shmakovpn <shmakovpn@yandex.ru>
 # Date: 2021-03-23
 SCRIPT_DIR=$(echo $(dirname $(realpath $0)))
-sassc "${SCRIPT_DIR}/bem/src/scss/style.scss" "${SCRIPT_DIR}/bem/css/style.css"
+PROJECT_NAME="css_like_js"
+sassc "${SCRIPT_DIR}/${PROJECT_NAME}/src/scss/style.scss" "${SCRIPT_DIR}/${PROJECT_NAME}/css/style.css"
